f8fa3acbb37ef50e2ef1eef2d3e3ea50baa3c880
Jason Hobbs <jason.hobbs@calxeda.com>
2011-10-12 16:11:57 -0500

    sdr: add docstrings

--- pyipmi/sdr.py
+++ pyipmi/sdr.py
@@ -1,8 +1,17 @@
+"""SDR records
+
+These are really bare right now - look at the SDR commands to see
+how they actually get filled out.
+"""
+
 class Sdr(object):
+    """Base SDR record type for others to inherit"""
     pass
 
 class AnalogSdr(Sdr):
+    """An analog SDR record"""
     pass
 
 class DiscreteSdr(Sdr):
+    """A discrete SDR record"""
     pass
