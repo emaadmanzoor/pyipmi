69196cb244d0369ef7c4ad32ab4964e4fefb0878
Jason Hobbs <jason.hobbs@calxeda.com>
2011-10-18 21:04:40 -0500

    setup: use setuptools to provide setup
    This provides the 'setup.py develop' command, which eliminates the need
    for the hacky extra __init__.py in the tld.

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""This __init__.py makes it easier to include this from ~/python"""
-
-from pyipmi import *
-from pyipmi.bmc import *
-from pyipmi.server import *
-from pyipmi.commands import *
-from pyipmi.tools import *
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='pyipmi',
-      version='0.0.1',
+      version='0.0.2',
       packages=['pyipmi',
                 'pyipmi.commands',
-                'pyipmi.tools',
-                'pyipmi'],
+                'pyipmi.tools'],
 )
