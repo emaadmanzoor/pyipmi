8e55d74ebbfc8777ee44ef6fcc6666d3ce4f4203
Jason Hobbs <jason.hobbs@calxeda.com>
2011-11-16 11:22:09 -0600

    sdr: fixup parsing for sdr commands

--- pyipmi/commands/sdr.py
+++ pyipmi/commands/sdr.py
@@ -19,6 +19,13 @@ class SdrListCommand(Command, IpmitoolCommandMixIn):
     ipmitool_parse_response = IpmitoolCommandMixIn.parse_colon_record_list
     ipmitool_args = ['-v', 'sdr', 'list', 'all']
 
+    def sensor_name_parser(string):
+        return string.split('(')[0].strip()
+
+    def entity_id_parser(string):
+        m = re.search('(\d.\d{1,2})', string)
+        return m.groups()[0]
+
     def ipmitool_types(self, record):
         """Only matches Analog sensors right now.
 
@@ -30,25 +37,25 @@ class SdrListCommand(Command, IpmitoolCommandMixIn):
         else:
             return None, None
 
-    analog_response_fields = {
-        """
-        Unparsed fields for analog sensors:
+    """
+    Unparsed fields for analog sensors:
 
-         Readable Thresholds   : lnr lcr lnc unc ucr unr 
-         Settable Thresholds   : lnr lcr lnc unc ucr unr 
-         Threshold Read Mask   : lnr lcr lnc unc ucr unr 
-         Assertion Events      : 
-         Assertions Enabled    : unc+ ucr+ unr+ 
-         Deassertions Enabled  : unc+ ucr+ unr+ 
-        'Sensor ID' : {
-            'attr' : ('sensor_name', 'sensor_id'),
-            'parser' : PAREN_PAIR_VAL
+     Readable Thresholds   : lnr lcr lnc unc ucr unr 
+     Settable Thresholds   : lnr lcr lnc unc ucr unr 
+     Threshold Read Mask   : lnr lcr lnc unc ucr unr 
+     Assertion Events      : 
+     Assertions Enabled    : unc+ ucr+ unr+ 
+     Deassertions Enabled  : unc+ ucr+ unr+
+    """
+    analog_response_fields = {
+        'Sensor ID'             : { 
+                'attr' : 'sensor_name',
+                'parser' : sensor_name_parser
         },
-        'Entity ID' : {
-            'attr' : ('entity_id', 'entity_name'),
-            'parser' : PAREN_PAIR_VAL
+        'Entity ID'             : { 
+                'attr' : 'entity_id',
+                'parser' : entity_id_parser
         },
-        """
         'Sensor Type (Analog)'  : { 'attr' : 'sensor_type' },
         'Sensor Reading'        : {},
         'Status'                : {},
