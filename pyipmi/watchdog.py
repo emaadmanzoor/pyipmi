a17a91e5163ed19ebffe301c87b3a670a53c55b5
David Schwarz <david.schwarz@calxeda.com>
2012-02-08 14:32:38 -0600

    Add watchdog result types

---
+++ pyipmi/watchdog.py
@@ -0,0 +1,17 @@
+"""ipmitool mc watchdog Results
+
+"""
+
+class WatchdogGetResult(object):
+    """Object to hold watchdog get results"""
+    pass
+
+
+class WatchdogResetResult(object):
+    """Object to hold watchdog reset results"""
+    pass
+
+
+class WatchdogOffResult(object):
+    """Object to hold watchdog off results"""
+    pass
