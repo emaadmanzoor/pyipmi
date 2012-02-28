6830ff5f36ef1427520e1d330e60326c6efa64df
David Schwarz <david.schwarz@calxeda.com>
2012-02-28 17:01:50 -0600

    user commands -- missing file

---
+++ pyipmi/user.py
@@ -0,0 +1,28 @@
+"""ipmitool user Results
+
+"""
+
+class UserListResults(object):
+    """Object to hold user list [channel] results"""
+    pass
+
+
+class UserSetNameResults(object):
+    """Object to hold user set name results"""
+    pass
+
+class UserSetPasswordResults(object):
+    """Object to hold user set name results"""
+    pass
+
+class UserEnableResults(object):
+    """Object to hold user set name results"""
+    pass
+
+class UserDisableResults(object):
+    """Object to hold user set name results"""
+    pass
+
+class UserPrivResults(object):
+    """Object to hold user set name results"""
+    pass
