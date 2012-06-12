18eacb15d40ef2ff0e6ecfc2749a09464801f102
Jason Hobbs <jason.hobbs@calxeda.com>
2012-06-12 13:48:42 -0500

    improve the package building
    in preperation for external publication as a package

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
-setup(name='pyipmi',
-      version='0.0.2',
-      packages=['pyipmi',
+setup(
+    name='pyipmi',
+    version='0.1.0',
+    packages=['pyipmi',
                 'pyipmi.commands',
                 'pyipmi.tools'],
+    description='Wrapper for IPMI clients',
+    author='Calxeda',
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 2.7']
 )
