e163e1ec072537bfa0e705a84aee7f2f83de9420
Jason Hobbs <jason.hobbs@calxeda.com>
2012-07-10 18:28:49 -0500

    bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup
 
 setup(
     name='pyipmi',
-    version='0.1.0',
+    version='0.2.0',
     packages=['pyipmi',
                 'pyipmi.commands',
                 'pyipmi.tools'],
