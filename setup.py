bb18e2241e8117aaa6451fae3e9f4ed0262472ad
George Kraft <george.kraft@calxeda.com>
2012-09-20 13:30:40 -0500

    bump to version 0.5.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup
 
 setup(
     name='pyipmi',
-    version='0.4.0',
+    version='0.5.0',
     packages=['pyipmi',
                 'pyipmi.commands',
                 'pyipmi.tools'],
