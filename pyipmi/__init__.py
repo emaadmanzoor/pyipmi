8f5bb671e914d7958498ab0e43ed4c2aafb51a91
George Kraft <george.kraft@calxeda.com>
2013-12-19 09:17:37 -0600

    nojira: bump to pyipmi v0.11.0
    Skipping 0.10 entirely so that version numbering is in sync with
    cxmanage.

--- pyipmi/__init__.py
+++ pyipmi/__init__.py
@@ -32,7 +32,7 @@
 """pyipmi provides IPMI client functionality"""
 from __future__ import print_function
 
-__version__ = "0.9.2"
+__version__ = "0.11.0"
 
 __all__ = ['Handle', 'Tool', 'Command', 'make_bmc', 'IpmiError']
 
